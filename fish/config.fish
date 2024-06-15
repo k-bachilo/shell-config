74277eb260017c0838d9da3a5f7e1be99ebd2047
k-bachilo <79043013+k-bachilo@users.noreply.github.com>
2024-06-15 21:00:58 +0300

    Added Golang aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+# Httpie
 function hg
     http GET ":$argv[1]" $argv[2..-1]
 end
@@ -16,4 +17,29 @@ end
 
 function ht
     http PATCH ":$argv[1]" $argv[2..-1]
+end
+
+# Golang
+function g
+    set current_dir ""
+    for arg in $argv
+        if test $arg = ","
+            set current_dir ""
+        else if string match -qr "/" $arg
+            set current_dir $arg
+            mkdir -p $current_dir
+        else if test -n $current_dir
+            touch "$current_dir/$arg.go"
+        else
+            touch "$arg.go"
+        end
+    end
+end
+
+function gg
+    go get $argv
+end
+
+function gmi
+    go mod init $argv
 end
